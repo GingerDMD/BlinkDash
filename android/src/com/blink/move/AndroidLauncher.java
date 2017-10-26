3e2dea12e937700268d4fff921ac1f1c389c0f46
Preston Wilson <prestonawilson@gmail.com>
2017-10-25 22:35:36 -0400

    libGDX generated files

---
+++ android/src/com/blink/move/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.blink.move;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.blink.move.BlinkMove;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BlinkMove(), config);
+	}
+}
